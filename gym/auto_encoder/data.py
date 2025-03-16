4166c3380bfb50fa1370b10b02f38fbe65d44b27
zzzanghun <wlckd5660@gmail.com>
2025-03-16 17:42:02 +0900

    Remove experiment_origin.py script and update dataset loading in CostmapDataset
    - Delete the `experiment_origin.py` script, which contained the main experiment logic for trajectory prediction and evaluation.
    - Update `CostmapDataset` to load a combined dataset from a relative path using `PROJECT_PATH`.
    - Refactor trajectory processing to include action discretization and normalization, as well as adjustments to observation data for improved feature extraction.

--- gym/auto_encoder/data.py
+++ gym/auto_encoder/data.py
@@ -2,9 +2,13 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 import pickle
+import time
+import os
+
 np.set_printoptions(threshold=np.inf)
 np.set_printoptions(linewidth=np.inf)
 
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 class CostmapDataset(Dataset):
     """
@@ -21,38 +25,56 @@ class CostmapDataset(Dataset):
         """
         self.data = []
         
-        for i in range(1, 100):
-            dataset_path = f'/home/zzzanghun/git/decision-transformer/gym/data/ego/odom_400/ego-planner-data_{i}.pkl'
-            if i == 1:
-                with open(dataset_path, 'rb') as f:
-                    trajectories = pickle.load(f)
-            else:
-                with open(dataset_path, 'rb') as f:
-                    trajectories += pickle.load(f)
-        # for i in range(1, 30):
-        #     dataset_path = f'/home/zzzanghun/git/decision-transformer/gym/data/medial/grid_4/ego-planner-data_{i}.pkl'
-        #     with open(dataset_path, 'rb') as f:
-        #         trajectories += pickle.load(f)
-        # for i in range(1, 50):
-        #     dataset_path = f'/home/zzzanghun/git/decision-transformer/gym/data/medial/grid_5/ego-planner-data_{i}.pkl'
-        #     with open(dataset_path, 'rb') as f:
-        #         trajectories += pickle.load(f)
-        # for i in range(1, 100):
-        #     dataset_path = f'/home/zzzanghun/git/decision-transformer/gym/data/ego/odom_300/ego-planner-data_{i}.pkl'
-        #     with open(dataset_path, 'rb') as f:
-        #         trajectories += pickle.load(f)
+        dataset_path = f'{PROJECT_PATH}/data/combined_data.pkl'
+        print(PROJECT_PATH)
+        with open(dataset_path, 'rb') as f:
+            trajectories = pickle.load(f)
+
+        action_indices = [0, 1, 2, 6, 7, 8]
+        obs_indices = [0, 1, 3, 4, 6, 7, 9, 10]
         
-        for trajectory in trajectories:
-            for obs in trajectory['observations']:
-                # obs.shape 가 [C, 100*100 + ?] 형태라고 가정 (C=1 등)
-                # 앞 100*100 부분만 costmap으로 사용
-                costmap_1d = obs[:, :100*100]  # shape: (C, 10000)
-                costmap_2d = costmap_1d.reshape(100, 100)  # (100, 100)
-                
-                # float32 변환
-                costmap_2d = costmap_2d.astype(np.float32)
-                
-                self.data.append(costmap_2d)
+        for i in range(len(trajectories)):
+            trajectories[i]['actions'] = trajectories[i]['actions'][:, action_indices]
+            obs_first_part = trajectories[i]['observations'][:, :, :100*100]
+            obs_second_part = trajectories[i]['observations'][:, :, 100*100:]
+            obs_second_part = obs_second_part[:, :, obs_indices]
+            trajectories[i]['observations'] = np.concatenate([obs_first_part, obs_second_part], axis=2)
+            trajectories[i]['rewards'] = np.zeros(len(trajectories[i]['actions']), dtype=float)
+            save_traj = False
+            del_traj = False
+            for j in range(len(trajectories[i]['actions'])):
+                coef = trajectories[i]['actions'][j]
+                # Discretize to 0.001 intervals
+                coef = np.round(coef / 0.001) * 0.001
+                # Assign back
+                trajectories[i]['actions'][j] = coef
+                a5, a4, a3, b5, b4, b3 = coef
+                drone_info = trajectories[i]['observations'][j][:, 100*100:]
+                v_x = drone_info[0][2]         # 현재 x축 속도
+                v_y = drone_info[0][3]         # 현재 y축 속도
+                a_x = drone_info[0][6]         # 현재 x축 가속도
+                a_y = drone_info[0][7]         # 현재 y축 가속도
+                direction_vector = trajectories[i]['observations'][j][:, 100*100:100*100 + 2]
+                norm = np.linalg.norm(direction_vector)
+                if norm != 0:
+                    direction_vector = direction_vector / norm
+                trajectories[i]['observations'][j][:, 100*100:100*100 + 2] = direction_vector
+                obs_observation = trajectories[i]['observations'][j][:, :100*100].reshape(100, 100)
+                x0, y0 = 5, 5
+                t_values = np.arange(0, 1.5 + 0.01, 0.01)
+                for t in t_values:
+                    x = x0 + v_x * t + 0.5 * a_x * t**2 + a3 * t**3 + a4 * t**4 + a5 * t**5
+                    y = y0 + v_y * t + 0.5 * a_y * t**2 + b3 * t**3 + b4 * t**4 + b5 * t**5
+                    
+                    # grid map의 인덱스로 변환 (여기서는 반올림하여 정수 인덱스로 변환)
+                    ix = int(round(50 - (x - x0) * 10))
+                    iy = int(round(50 - (y - y0) * 10))
+                    
+                    # grid map의 범위 내에 있는 경우에만 값을 2로 지정
+                    if 0 <= ix < 100 and 0 <= iy < 100:
+                        obs_observation[ix, iy] = 0.5  # 일반적으로 행이 y축, 열이 x축을 나타냄
+
+                self.data.append(obs_observation.copy())
 
     def __len__(self):
         return len(self.data)
