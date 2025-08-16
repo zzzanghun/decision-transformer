a06fa98691e88b6c30e9df63dd56c9674fcf9b7b
zzzanghun <wlckd5660@gmail.com>
2025-08-16 01:10:23 +0000

    ...

--- gym/decision_transformer/training/seq_trainer.py
+++ gym/decision_transformer/training/seq_trainer.py
@@ -36,7 +36,7 @@ class SequenceTrainer(Trainer):
 
         self.optimizer.zero_grad()
         loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), .25)
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), .5)
         self.optimizer.step()
 
         # with torch.no_grad():
