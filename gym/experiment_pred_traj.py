de00a048a9d4066601400d7b19595b43e376cf7c
zzzanghun <wlckd5660@gmail.com>
2025-08-18 00:32:44 +0000

    ...

--- gym/experiment_pred_traj.py
+++ gym/experiment_pred_traj.py
@@ -571,7 +571,7 @@ if __name__ == '__main__':
     parser.add_argument('--env', type=str, default='ego-planner')
     parser.add_argument('--dataset', type=str, default='medium')  # medium, medium-replay, medium-expert, expert
     parser.add_argument('--mode', type=str, default='normal')  # normal for standard setting, delayed for sparse
-    parser.add_argument('--K', type=int, default=5)
+    parser.add_argument('--K', type=int, default=30)
     parser.add_argument('--pct_traj', type=float, default=1.)
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--model_type', type=str, default='dt')  # dt for decision transformer, bc for behavior cloning
