738c54482c2a050f609b768226c0cb1cda9aa797
zzzanghun <wlckd5660@gmail.com>
2025-08-22 02:21:55 +0000

    ...

--- gym/experiment_pred_traj.py
+++ gym/experiment_pred_traj.py
@@ -624,7 +624,7 @@ if __name__ == '__main__':
     parser.add_argument('--dropout', type=float, default=0.1)
     parser.add_argument('--learning_rate', '-lr', type=float, default=1e-4)
     parser.add_argument('--weight_decay', '-wd', type=float, default=0.03)
-    parser.add_argument('--warmup_steps', type=int, default=10000)
+    parser.add_argument('--warmup_steps', type=int, default=50)
     parser.add_argument('--num_eval_episodes', type=int, default=100)
     parser.add_argument('--max_iters', type=int, default=500000)
     parser.add_argument('--num_steps_per_iter', type=int, default=100)
