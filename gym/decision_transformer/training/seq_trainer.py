c0200f9859ca660778a4680c49fc8d48b1c9216e
zzzanghun <wlckd5660@gmail.com>
2025-08-22 01:50:51 +0000

    ...

--- gym/decision_transformer/training/seq_trainer.py
+++ gym/decision_transformer/training/seq_trainer.py
@@ -36,7 +36,7 @@ class SequenceTrainer(Trainer):
 
         self.optimizer.zero_grad()
         loss.backward()
-        if iter_num > 500:
+        if iter_num > 50:
             grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
         else:
             grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 10.0)
