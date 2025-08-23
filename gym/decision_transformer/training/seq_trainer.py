7b24c591a33f9b20c6306d26fe9c6777186b166c
zzzanghun <wlckd5660@gmail.com>
2025-08-23 09:23:12 +0000

    beta 값 조정

--- gym/decision_transformer/training/seq_trainer.py
+++ gym/decision_transformer/training/seq_trainer.py
@@ -31,7 +31,7 @@ class SequenceTrainer(Trainer):
         kl = 0.5 * torch.sum(mu.pow(2) + logvar.exp() - logvar - 1.0, dim=-1)
         kl_loss = kl.mean()
 
-        beta_target = 0.1
+        beta_target = 0.3
         warmup_steps = 5000
         beta_kl = min(beta_target, beta_target * (self.train_num + 1) / warmup_steps)
 
