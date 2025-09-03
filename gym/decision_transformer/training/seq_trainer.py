cf25348c0de6a179be5f1ae97d777b40f554154d
zzzanghun <wlckd5660@gmail.com>
2025-09-03 06:17:36 +0000

    grad clip norm 조정

--- gym/decision_transformer/training/seq_trainer.py
+++ gym/decision_transformer/training/seq_trainer.py
@@ -12,10 +12,10 @@ class SequenceTrainer(Trainer):
         
         if self.train_num < 50000:
             current_batch_size = self.batch_size
-            grid_clip_value = 5.0
+            grad_clip_value = 3.0
         else:
             current_batch_size = self.batch_size / 8
-            grid_clip_value = 2.0
+            grad_clip_value = 1.5
 
         states, actions, rewards, dones, rtg, timesteps, attention_mask, odom = self.get_batch(batch_size=current_batch_size, num_iter=self.train_num)
         action_target = torch.clone(actions)
@@ -55,9 +55,9 @@ class SequenceTrainer(Trainer):
         self.optimizer.zero_grad()
         loss.backward()
         if iter_num > 50:
-            grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), grid_clip_value)
+            grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), grad_clip_value)
         else:
-            grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 10.0)
+            grad_norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 20.0)
         self.optimizer.step()
 
         # with torch.no_grad():
